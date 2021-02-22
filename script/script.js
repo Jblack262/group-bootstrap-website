9730b7f62ca7f3f7623c854179aff29e477536e3
James Blackman <JBlack262@west-mec.org>
2021-02-22 16:52:10 -0700

    fixed navbar and added font

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 
-var pageNum = {one: '1', two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7};
-function scroll(pageNumber) {
+function scrollPage(pageNumber) {
     var elmnt = document.getElementById("main");
     var target = elmnt.clientHeight * (pageNumber - 1);
     elmnt.scrollTop = target;
