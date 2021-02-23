177721ab6b3a7e00fdb9fabc8e4acd3470f357c1
James Blackman <JBlack262@west-mec.org>
2021-02-22 17:57:24 -0700

    added darkmode button and functionality

--- script/script.js
+++ script/script.js
@@ -3,3 +3,18 @@ function scrollPage(pageNumber) {
     var target = elmnt.clientHeight * (pageNumber - 1);
     elmnt.scrollTop = target;
 }
+
+let buttons = [];
+buttons.push(document.getElementById("btn1"));
+buttons.push(document.getElementById("btn2"));
+buttons.push(document.getElementById("btn3"));
+buttons.push(document.getElementById("btn4"));
+buttons.push(document.getElementById("btn5"));
+buttons.push(document.getElementById("btn6"));
+buttons.push(document.getElementById("btn7"));
+
+function toggleDarkMode() {
+    for (var i = 0; i < buttons.length; i ++) {
+        buttons[i].classList.toggle("btn-Light");
+    }
+}
